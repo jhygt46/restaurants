405496455119a552110b308ffd089c2843195e84
jhygt46 <infolifeoperacional@gmail.com>
2018-09-21 15:49:52 -0300

    aa

--- index.php
+++ index.php
@@ -1,16 +1,121 @@
 <?php
-    session_start();
+
+if($_GET['accion'] == "actualizar"){
     
-    if($_GET["accion"] == "logout"){
-        session_destroy();
-        echo '<meta http-equiv="refresh" content="0; url=index.php">';
-        exit;
-    }
+    require('../class/core_class.php');
+    require('html/html.php');
+    $core = new Core();
+    $js = $core->get_web_js_data();
+    file_put_contents("js/info.js", "var data=".json_encode($js));
+    file_put_contents("js/html.js", "var html=".json_encode($html));
+    echo "Actualizado! redireccionando";
+    echo "<meta http-equiv='refresh' content='2; url=http://localhost/easyapps/web'>";
     
-    if(!isset($_SESSION['user']['info']['id_user'])){
-        include("login.php");
-    }else{
-        include("inicio.php");
-    }
+}
+
+require('../class/domain_class.php');
+$domain = new Dominio();
+$info = $domain->get_data();
 
 ?>
+
+<!DOCTYPE html PUBLIC "-//W3C//DTD XHTML 1.1//EN" "http://www.w3.org/TR/xhtml11/DTD/xhtml11.dtd">
+<html xmlns="http://www.w3.org/1999/xhtml">
+    <head>
+        <title>MIKA SUSHI</title>
+        <meta http-equiv="Content-Type" content="text/html; charset=utf-8" />
+        <meta name="viewport" content="width=device-width, initial-scale=1.0">
+        <link href="https://fonts.googleapis.com/icon?family=Material+Icons" rel="stylesheet">
+        <link href="https://fonts.googleapis.com/css?family=Titillium+Web" rel="stylesheet">
+        <link rel="stylesheet" href="css/style.css" media="all" />
+        <link rel="stylesheet" href="<?php echo $info["css_style"]; ?>" media="all" />
+        <link rel="stylesheet" href="<?php echo $info["css_color"]; ?>" media="all" />
+        <link rel="stylesheet" href="<?php echo $info["css_modals"]; ?>" media="all" />
+        <script src="js/jquery-1.3.2.min.js" type="text/javascript"></script>
+        <script src="js/info.js" type="text/javascript"></script>
+        <script src="js/html.js" type="text/javascript"></script>
+        <script src="js/html_func.js" type="text/javascript"></script>
+        <script src="js/base.js" type="text/javascript"></script>
+</head>
+    <body class="style_page_1">
+        <div class="contenedor">
+            <div class="menu_left">
+                <div class="cont_menu_left">
+                    <div class="btn_toogle color_font_01 material-icons" onclick="tooglemenu()">view_headline</div>
+                    <div class="menu_info">
+                        <h1>Men&uacute;</h1>
+                        <ul>
+                            <li>Sushi para Embarazadas</li>
+                        </ul>
+                    </div>
+                </div>
+            </div>
+            <div class="pagina">
+                <div class="header color_back_03 fixed">
+                    <div class="header_logo vhalign"><img src="images/logo.png" alt="" /></div>
+                    <div class="menu_right" onclick="open_carro()"><div class="shop color_font_01 material-icons">shopping_cart</div><div class="cantcart color_back_02"><div class="cantcart_num vhalign size_font_02 color_font_02">15</div></div></div>
+                </div>
+                <div class="contenido color_back_01">
+                    <div class="cont_contenido"></div>
+                </div>
+                <div class="footer fixed color_back_03"></div>
+            </div>
+            <div class="modals">
+                <div class="cont_modals">
+                    <div class="modal vhalign hide modal_carta">
+                        <div class="cont_modal">
+                            <div class="titulo"><div class="cont_titulo valign"><h1 class="size_font_03 color_font_03"></h1><h2 class="size_font_02 color_font_02"></h2></div></div>
+                            <div class="close"></div>
+                            <div class="cont_info">
+                                <div class="info_modal"></div>
+                            </div>
+                        </div>
+                    </div>
+                    <div class="modal vhalign hide modal_carro">
+                        <div class="cont_modal">
+                            <div class="titulo"><div class="cont_titulo valign"><h1 class="size_font_03 color_font_03"></h1><h2 class="size_font_02 color_font_02"></h2></div></div>
+                            <div class="close"></div>
+                            <div class="cont_info">
+                                <div class="info_modal carro_inicio"></div>
+                                <div class="info_modal carro_direccion" style="display: none">INFO MODAL DATOS</div>
+                                <div class="info_modal carro_final" style="display: none">INFO MODAL FINAL</div>
+                            </div>
+                            <div class="acciones">
+                                <input class="confirmar" onclick="confirmar_pedido()" type="button" value="Confirmar" />
+                            </div>
+                        </div>
+                    </div>
+                    <div class="modal vhalign hide modal_productos_promo">
+                        <div class="cont_modal">
+                            <div class="cont_modal">
+                            <div class="titulo"><div class="cont_titulo valign"><h1 class="size_font_03 color_font_03"></h1><h2 class="size_font_02 color_font_02"></h2></div></div>
+                            <div class="close"></div>
+                            <div class="cont_info">
+                                <div class="info_modal"></div>
+                            </div>
+                            <div class="acciones">
+                                <input class="confirmar" onclick="confirmar_productos_promo(this)" type="button" value="Confirmar" />
+                            </div>
+                        </div>
+                        </div>
+                    </div>
+                    <div class="modal vhalign hide modal_pregunta_productos">
+                        <div class="cont_modal">
+                            <div class="cont_modal">
+                            <div class="titulo"><div class="cont_titulo valign"><h1 class="size_font_03 color_font_03"></h1><h2 class="size_font_02 color_font_02"></h2></div></div>
+                            <div class="close"></div>
+                            <div class="cont_info">
+                                <div class="info_modal"></div>
+                            </div>
+                            <div class="acciones">
+                                <input class="confirmar" onclick="confirmar_pregunta_productos(this)" type="button" value="Confirmar" />
+                            </div>
+                        </div>
+                        </div>
+                    </div>
+                </div>
+            </div>
+        </div>
+        <!--<script src="https://maps.googleapis.com/maps/api/js?key=AIzaSyDbKlHezhqgy7z57ipcJk8mDK4rf6drvjY&libraries=places" async defer></script>-->
+    </body>
+</html>
