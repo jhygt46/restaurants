6a39db50a561556bcb9492f16458c689b1286303
jhygt46 <infolifeoperacional@gmail.com>
2018-09-28 16:46:01 -0300

    aa

--- index.php
+++ index.php
@@ -24,10 +24,10 @@ if(isset($_GET['param_dom'])){
         <link rel="stylesheet" href="<?php echo $info["css_color"]; ?>" media="all" />
         <link rel="stylesheet" href="<?php echo $info["css_modals"]; ?>" media="all" />
         <script src="/js/jquery-1.3.2.min.js" type="text/javascript"></script>
-        <script src="/js/data/<?php echo $info["code"]; ?>.js" type="text/javascript"></script>
-        <script src="/js/html/<?php echo $info["code"]; ?>.js" type="text/javascript"></script>
-        <script src="/js/html_func.js" type="text/javascript"></script>
-        <script src="/js/base.js" type="text/javascript"></script>
+        <script src="<?php echo $info["js_data"]; ?>" type="text/javascript"></script>
+        <script src="<?php echo $info["js_data"]; ?>" type="text/javascript"></script>
+        <script src="<?php echo $info["js_html_func"]; ?>" type="text/javascript"></script>
+        <script src="<?php echo $info["js_base"]; ?>" type="text/javascript"></script>
         <style>
             body{
                 font-family: <?php echo $info["font"]['css']; ?>;
