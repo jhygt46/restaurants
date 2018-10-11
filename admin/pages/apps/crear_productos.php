9bdc2503bd2b2badb37b509162f563ae6eb61124
jhygt46 <infolifeoperacional@gmail.com>
2018-10-11 16:02:03 -0300

    aa

--- admin/pages/apps/crear_productos.php
+++ admin/pages/apps/crear_productos.php
@@ -176,7 +176,7 @@ if(isset($_GET["id"]) && is_numeric($_GET["id"]) && $_GET["id"] != 0){
                 
                 <li class="user">
                     <ul class="clearfix">
-                        <li class="nombre"><?php echo $nombre; ?> / <?php echo $id_n; ?></li>
+                        <li class="nombre"><?php echo $nombre; ?></li>
                         <a title="Eliminar" class="icn borrar" onclick="eliminar('<?php echo $eliminaraccion; ?>', '<?php echo $id; ?>/<?php echo $id_n; ?>', '<?php echo $eliminarobjeto; ?>', '<?php echo $nombre; ?>')"></a>
                         <a title="Modificar" class="icn modificar" onclick="navlink('<?php echo $page_mod; ?>?id=<?php echo $id; ?>&id_pro=<?php echo $id_n; ?>')"></a>
                         <a title="Configurar Producto" class="icn conficon" onclick="navlink('pages/apps/configurar_producto.php?id_pro=<?php echo $id_n; ?>&nombre=<?php echo $nombre; ?>&id=<?php echo $id; ?>')"></a>
