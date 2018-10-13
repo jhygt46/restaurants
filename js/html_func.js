1d4eb0ea62d6269709d81f064abfe5d0c0158bd4
jhygt46 <infolifeoperacional@gmail.com>
2018-10-13 15:51:49 -0300

    aa

--- js/html_func.js
+++ js/html_func.js
@@ -262,8 +262,8 @@ function create_html_categorias(obj){
     
 }
 function view_product(that){
-    console.log(that.parentElement.childNodes);
-    console.log(that.parentNode.childNodes);
+    console.log(that.parentElement.childNodes[1].style.display);
+    that.parentNode.childNodes[1].style.display = 'block';
 }
 function create_html_producto(id){
     
