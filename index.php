831868de858938ca4d1d43c6a38caf0302962795
jhygt46 <infolifeoperacional@gmail.com>
2018-10-13 17:15:57 -0300

    aa

--- index.php
+++ index.php
@@ -39,7 +39,7 @@ $core->get_web_js_data($info['id_gir']);
         <div class="contenedor">
             <div class="menu_left">
                 <div class="cont_menu_left">
-                    <div class="btn_toogle color_font_01 material-icons" onclick="tooglemenu()">view_headline</div>
+                    <div class="btn_toogle color_icons material-icons" onclick="tooglemenu()">view_headline</div>
                     <div class="menu_info">
                         <h1>Men&uacute;</h1>
                         <ul class="lista_paginas">
@@ -51,7 +51,7 @@ $core->get_web_js_data($info['id_gir']);
             <div class="pagina">
                 <div class="header color_header <?php echo ($info["header_fixed"] == 1) ? 'fixed' : ''; ?>">
                     <div class="header_logo vhalign"><img src="/images/logos/<?php echo $info["logo"]; ?>" alt="" /></div>
-                    <div class="menu_right" onclick="open_carro()"><div class="shop color_font_01 material-icons">shopping_cart</div><div class="cantcart color_back_02"><div class="cantcart_num vhalign size_font_02 color_font_02">15</div></div></div>
+                    <div class="menu_right" onclick="open_carro()"><div class="shop color_icons material-icons">shopping_cart</div><div class="cantcart color_back_02"><div class="cantcart_num vhalign size_font_02 color_font_02">15</div></div></div>
                 </div>
                 <div class="contenido color_pagina">
                     <div class="cont_contenido"></div>
