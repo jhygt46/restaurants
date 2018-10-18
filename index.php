af25cb9e85544d150cf3ebc32e8b70a03428117d
jhygt46 <infolifeoperacional@gmail.com>
2018-10-18 17:14:02 -0300

    aa

--- index.php
+++ index.php
@@ -10,8 +10,7 @@ if(isset($_GET['param_dom'])){
 }
 
 $inf = $core->get_web_js_data2($info['id_gir']);
-print_r($inf);
-exit;
+
 ?>
 
 <!DOCTYPE html PUBLIC "-//W3C//DTD XHTML 1.1//EN" "http://www.w3.org/TR/xhtml11/DTD/xhtml11.dtd">
