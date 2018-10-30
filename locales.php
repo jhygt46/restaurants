2bf847511f371569401f8776d7bc49ae5168dc16
jhygt46 <infolifeoperacional@gmail.com>
2018-10-30 18:34:17 -0300

    aa

--- locales.php
+++ locales.php
@@ -10,10 +10,6 @@ if(isset($_GET['param_dom'])){
     $info = $core->get_data('www.runasushi.cl');
 }
 
-echo "<pre>";
-print_r($info);
-echo "</pre>";
-
 ?>
 <!DOCTYPE html PUBLIC "-//W3C//DTD XHTML 1.1//EN" "http://www.w3.org/TR/xhtml11/DTD/xhtml11.dtd">
 <html xmlns="http://www.w3.org/1999/xhtml">
