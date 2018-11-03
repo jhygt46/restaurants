4b5c0ae097b0c326c346c17061017b73e689cbd5
jhygt46 <infolifeoperacional@gmail.com>
2018-11-03 18:07:38 -0300

    aa

--- admin/class/restaurant_class.php
+++ admin/class/restaurant_class.php
@@ -15,10 +15,13 @@ class Rest{
     }
     public function get_info(){
         
+        $accion = $_POST["accion"];
+        
         $info['op'] = 2;
+        $info['accion'] = $accion;
         return $info;
         
-        $accion = $_POST["accion"];
+        
         if($accion == "enviar_pedido"){
         
         }
