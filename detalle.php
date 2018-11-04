485f92ceea1b2484ed8276fbabf87dcf8d5a5668
jhygt46 <infolifeoperacional@gmail.com>
2018-11-03 21:24:55 -0300

    aa

--- detalle.php
+++ detalle.php
@@ -6,7 +6,6 @@ $core = new Core();
 if(isset($_GET['code'])){
     
     $info = $core->ver_detalle($_GET['code']);
-    print_r($info);
 
 ?>
 <!DOCTYPE html PUBLIC "-//W3C//DTD XHTML 1.1//EN" "http://www.w3.org/TR/xhtml11/DTD/xhtml11.dtd">
