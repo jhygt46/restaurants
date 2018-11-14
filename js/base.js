a368b1b931cefa9e88bd0158859c1f69d639fa77
jhygt46 <infolifeoperacional@gmail.com>
2018-11-14 17:28:54 -0300

    aa

--- js/base.js
+++ js/base.js
@@ -197,6 +197,9 @@ function ver_pagina(id){
             $('.modal_pagina .titulo h1').html(data.paginas[i].titulo);
             $('.modal_pagina .titulo h2').html(data.paginas[i].subtitulo);
             $('.modal_pagina .info_modal').html(data.paginas[i].html);
+            
+            console.log(data.paginas[i]);
+            
             show_modal('modal_pagina');
         }
     }
