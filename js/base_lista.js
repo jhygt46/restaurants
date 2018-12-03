20ca886b13a1f332807f045a6553ef1731f4ad5a
jhygt46 <infolifeoperacional@gmail.com>
2018-12-03 16:11:12 -0300

    aa

--- js/base_lista.js
+++ js/base_lista.js
@@ -141,6 +141,11 @@ function add_carro_promocion(id_cae){
     set_cantidad(1);
     localStorage.setItem("carro", JSON.stringify(carro));
     
+    if(proceso(true, false)){
+        process_carro();
+        show_modal('paso_01');
+    }
+    /*
     var pro = process_new_promos();
     for(var i=pro.carro_promos.length-1; i>=0; i--){
         if(pro.carro_promos[i].id_cae == id_cae){
@@ -148,9 +153,8 @@ function add_carro_promocion(id_cae){
             return;
         }
     }
+    */
     
-    //process_carro();
-    show_modal('paso_01');
     
 }
 function tiene_pregunta(carro){
