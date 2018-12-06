a646880f21221b7ca75a890dcedd9962c3a9f9ab
jhygt46 <infolifeoperacional@gmail.com>
2018-12-06 17:34:37 -0300

    aa

--- admin/pages/base/ver_informe.php
+++ admin/pages/base/ver_informe.php
@@ -21,13 +21,19 @@ if(isset($_GET["id_gir"]) && is_numeric($_GET["id_gir"]) && $_GET["id_gir"] != 0
     $fireapp->is_giro($id_gir);
     $giro = $fireapp->get_giro();
     
-    
     $giro_nombre = $giro['nombre'];
     $giro_dominio = $giro['dominio'];
     
-    echo file_get_contents("http://".$giro_dominio."?accion=verificar_dominio_online");
-    echo "<br>";
-    echo file_get_contents("https://".$giro_dominio."?accion=verificar_dominio_online");
+    $http_code = file_get_contents("http://".$giro_dominio."?accion=verificar_dominio_online");
+    $https_code = file_get_contents("https://".$giro_dominio."?accion=verificar_dominio_online");
+    
+    if($http_code == "X403-Y202-Z703"){
+        echo "SITIO SIN SEGURIDAD ARRIBA";
+    }
+    if($https_code == "X403-Y202-Z703"){
+        echo "SITIO CON SEGURIDAD ARRIBA";
+    }
+    
     
 }
 
