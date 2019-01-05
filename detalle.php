60389cf65ffc101e293c29ef757a2c0ca221fc90
jhygt46 <infolifeoperacional@gmail.com>
2019-01-05 17:23:45 -0300

    aa

--- detalle.php
+++ detalle.php
@@ -72,7 +72,7 @@ if(isset($_GET['code'])){
                 <?php if($info['pre_palitos'] == 1){ ?><div class="txtcen font_04">Palitos</div><?php } ?>
                 <?php if($info['pre_soya'] == 1){ ?><div class="txtcen font_04">Soya</div><?php } ?>
                 <?php if($info['pre_teriyaki'] == 1){ ?><div class="txtcen font_04">Teriyaki</div><?php } ?>
-                <?php if($info['comentarios'] != ""){ ?><div style="padding-top: 6px" class="txtcen font_02"><?php echo $info['comentarios']; ?></div><?php } ?>
+                <?php if($info['comentarios'] != ""){ ?><div style="padding-top: 10px" class="txtcen font_02"><?php echo $info['comentarios']; ?></div><?php } ?>
                 
             </div>
             <?php } ?>            
