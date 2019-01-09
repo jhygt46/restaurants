294b1e1719b07a60efe92cdb19dc11d81a4ce880
jhygt46 <infolifeoperacional@gmail.com>
2019-01-09 15:18:35 -0300

    aa

--- js/base_lista.js
+++ js/base_lista.js
@@ -704,7 +704,7 @@ function show_modal_4(pedido){
     
     map_socket = new google.maps.Map(document.getElementById('mapa_posicion'), {
         center: punto,
-        zoom: 13,
+        zoom: 8,
         mapTypeId: 'roadmap',
         disableDefaultUI: true
     });
