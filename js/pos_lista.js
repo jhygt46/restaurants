d84aeb7791f1543460dd6caa90fd042a711741e1
jhygt46 <infolifeoperacional@gmail.com>
2019-01-10 18:04:34 -0300

    aa

--- js/pos_lista.js
+++ js/pos_lista.js
@@ -120,8 +120,8 @@ function modificar_horas(){
             var fecha_ahora = Math.round(new Date().getTime()/1000);
             var time = (pedidos[i].despacho == 1) ? tiempos.despacho : tiempos.retiro ;
             var diff = Math.round((pedidos[i].fecha + time - fecha_ahora)/60);
-            console.log(fecha_ahora);
-            console.log(pedidos[i].fecha);
+            var d = fecha_ahora - pedidos[i].fecha;
+            console.log(d);
             $('.lista_pedidos').find('.pedido').eq(i).find('.t_tiempo').find('.t_nombre').html(diff);
 
         }
