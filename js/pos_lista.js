619e8290ed7a63cca7d92119b2c43495876f82f2
jhygt46 <infolifeoperacional@gmail.com>
2019-01-14 12:00:53 -0300

    aa

--- js/pos_lista.js
+++ js/pos_lista.js
@@ -246,7 +246,7 @@ function html_home_pedidos(obj, index){
     
     if(obj.carro){
         obj.carro.forEach(function(carro_item, index){
-            if(carro_item.id_pro){
+            if(carro_item.id_pro && !carro_item.promo){
                 pro = get_producto(carro_item.id_pro);
                 total = total + parseInt(pro.precio);
             }
@@ -256,7 +256,6 @@ function html_home_pedidos(obj, index){
     if(obj.promos){
         obj.promos.forEach(function(promo_item, index){
             cat = get_categoria(promo_item.id_cae);
-            console.log(cat);
             total = total + parseInt(cat.precio);
         });
     }
