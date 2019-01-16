46fdffa86847847dee3febb4fa4987f50c85869e
jhygt46 <infolifeoperacional@gmail.com>
2019-01-16 18:08:37 -0300

    aa

--- admin/class/stats_class.php
+++ admin/class/stats_class.php
@@ -108,13 +108,8 @@ class Stats extends Core{
         $info['series'][] = $this->get_series($infos, 'Buena Nelson 2');
         $info['series'][] = $this->get_series($infos, 'Buena Nelson 3');
         
-        //$info['total'] = $this->pedidos_total_fecha($pedidos, '2019-01-01 00:00:00', '2019-02-01 00:00:00');
-        
-        for($i=0; $i<count($pedidos); $i++){
-            
-            $fecha_pedido = strtotime($pedidos[$i]['fecha']);
-            
-        }
+        $info['total'] = $this->pedidos_total_fecha($pedidos, '2019-01-01 00:00:00', '2019-02-01 00:00:00');
+
         
         /*
         for($i=0; $i<$pedidos['count']; $i++){
