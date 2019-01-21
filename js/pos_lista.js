1d80fa5edff0a3bcd0588c1d495283d88a898963
jhygt46 <infolifeoperacional@gmail.com>
2019-01-21 14:05:14 -0300

    aa

--- js/pos_lista.js
+++ js/pos_lista.js
@@ -31,6 +31,7 @@ function add_carro_producto(id_pro){
     
     pedido.carro.push(item_carro);
     set_pedidos(pedidos);
+    categorias_base(0);
     listar_pedidos();
 
 }
@@ -65,6 +66,7 @@ function add_carro_promocion(id_cae){
     }
 
     set_pedidos(pedidos);
+    categorias_base(0);
     listar_pedidos();
     
 }
