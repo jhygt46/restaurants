df9313444c541096e8efd6cffa6a1a1b38641b16
jhygt46 <infolifeoperacional@gmail.com>
2019-01-22 15:44:55 -0300

    aa

--- admin/class/restaurant_class.php
+++ admin/class/restaurant_class.php
@@ -226,8 +226,8 @@ class Rest{
             curl_setopt($ch, CURLOPT_URL, 'http://35.196.220.197/enviar_local');
             curl_setopt($ch, CURLOPT_RETURNTRANSFER, true);
             curl_setopt($ch, CURLOPT_POSTFIELDS, http_build_query($pedido));
-            $mail_nodejs = json_encode(curl_exec($ch));
-            $info['mail'] = $mail_nodejs;
+            $mail_nodejs = json_decode(curl_exec($ch));
+            $info['mail'] = ($mail_nodejs->{'op'} == 1) ? true : false ;
             curl_close($ch);
             
         }else{
