d4e44e8ce1c7fab798b68a0f6f483555f62cfcb9
jhygt46 <infolifeoperacional@gmail.com>
2019-01-24 10:36:43 -0300

    aa

--- detalle.php
+++ detalle.php
@@ -7,11 +7,11 @@ $core = new Core();
 if(isset($_GET['code'])){
     
     $info = $core->ver_detalle($_GET['code']);
-
+    /*
     echo "<pre>";
     print_r($info);
     echo "</pre>";
-    
+    */
     if($info['op']){
         
         $id_ped = $info["id_ped"];
