71afc106f33d87bc94c92091736350f99e7a0642
jhygt46 <infolifeoperacional@gmail.com>
2019-01-28 16:00:37 -0300

    aa

--- locales.php
+++ locales.php
@@ -135,9 +135,9 @@ $code = $core->socket_code($id_loc, $info['id_gir']);
                                 </div>
                                 <div class="preguntas">
                                     <div class="pregunta clearfix">
-                                        <div class="btn_pedido">Boton 1</div>
-                                        <div class="btn_pedido">Boton 2</div>
-                                        <div class="btn_pedido">Boton 3</div>
+                                        <div class="btn_pedido"><div class="btn_ped">Boton 1</div></div>
+                                        <div class="btn_pedido"><div class="btn_ped">Boton 2</div></div>
+                                        <div class="btn_pedido"><div class="btn_ped">Boton 3</div></div>
                                     </div>
                                 </div>
                             </div>
