3f1665ebdb0ec606efc4f932e50ba45a17cd607e
jhygt46 <infolifeoperacional@gmail.com>
2019-02-06 11:53:27 -0300

    aa

--- admin/index.php
+++ admin/index.php
@@ -1,6 +1,13 @@
 <?php
     session_start();
     
+    if (empty($_SERVER['HTTPS']) || $_SERVER['HTTPS'] === "off") {
+        $location = 'https://www.misitiodelivery.cl/admin';
+        header('HTTP/1.1 301 Moved Permanently');
+        header('Location: ' . $location);
+        exit;
+    }
+
     if($_GET["accion"] == "logout"){
         session_destroy();
         echo '<meta http-equiv="refresh" content="0; url=index.php">';
