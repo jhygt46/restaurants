6de313d7271acb8f137b05c488f783fd308b08d9
jhygt46 <infolifeoperacional@gmail.com>
2019-02-27 11:46:44 -0300

    aa

--- js/base_lista.js
+++ js/base_lista.js
@@ -989,6 +989,8 @@ function time(){
 }
 function open_socket(code){
     
+    console.log("SOCKET CONNECT");
+
     var socket = io.connect('https://www.izusushi.cl:443', { 'forceNew': true });
     socket.on('pedido-'+code, function(data){
 
