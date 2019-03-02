18c1ed25cc385dd2e2cd38ae3bf5c7624e793f48
jhygt46 <infolifeoperacional@gmail.com>
2019-03-02 16:42:39 -0300

    aa

--- admin/pages/msd/usuarios.php
+++ admin/pages/msd/usuarios.php
@@ -33,6 +33,10 @@ $list_giros = $fireapp->get_giros();
 $inicio = $fireapp->inicio();
 $m_locales = false;
 
+echo "<pre>";
+print_r($inicio);
+echo "</pre>";
+
 if(isset($_GET["id_user"]) && is_numeric($_GET["id_user"]) && $_GET["id_user"] != 0){
 
     $id_user = $_GET["id_user"];
