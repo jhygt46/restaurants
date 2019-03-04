a0b1501993e696c8a38adababc93c069c238f939
jhygt46 <infolifeoperacional@gmail.com>
2019-03-04 15:36:59 -0300

    aa

--- admin/class/restaurant_class.php
+++ admin/class/restaurant_class.php
@@ -161,7 +161,7 @@ class Rest{
                                 $send['code'] = $mailcode;
 
                                 $ch = curl_init();
-                                curl_setopt($ch, CURLOPT_URL, 'http://35.196.220.197/mail_inicio');
+                                curl_setopt($ch, CURLOPT_URL, 'https://www.izusushi.cl/mail_inicio');
                                 curl_setopt($ch, CURLOPT_RETURNTRANSFER, true);
                                 curl_setopt($ch, CURLOPT_POSTFIELDS, http_build_query($send));
                                 curl_exec($ch);
