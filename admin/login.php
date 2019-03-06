48179127a67b8341f4596a46e0c43a97bac242a0
jhygt46 <infolifeoperacional@gmail.com>
2019-03-06 11:38:35 -0300

    aa

--- admin/login.php
+++ admin/login.php
@@ -61,7 +61,7 @@ if($_GET["paso"] == "nueva"){
                             <div class='btn'><input type='button' id='recuperar' value='Entrar'></div>
                         </div>
                     </div>
-                    <div class='ltpass'><a href='/'>Deseo ingresar</a></div>
+                    <div class='ltpass'><a href='/admin'>Deseo ingresar</a></div>
                 <?php } ?>
                 <?php if(isset($_GET["paso"]) && $_GET["paso"] == "nueva"){ ?>
                     <div class='titulo'>NUEVA CONTRASEÑA</div>
