a7c6768112960b2fcef12443807777602cc45364
jhygt46 <infolifeoperacional@gmail.com>
2019-03-20 15:09:31 -0300

    aa

--- js/html_func.js
+++ js/html_func.js
@@ -498,6 +498,9 @@ function html_preguntas_producto(i){
                 
                 var n_pregunta = document.createElement('div');
                 if(carro[i].preguntas[k].valores[m].seleccionados){
+                    
+                    console.log(carro[i].preguntas[k].valores[m].seleccionados);
+                    console.log(carro[i].preguntas[k].valores[m].valores[n]);
 
                     if(carro[i].preguntas[k].valores[m].seleccionados.indexOf(carro[i].preguntas[k].valores[m].valores[n]) != -1){
                         n_pregunta.className = 'n_pregunta selected';
