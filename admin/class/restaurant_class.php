8b2a542d5d9918ba4acc59484617f6046947cad8
jhygt46 <infolifeoperacional@gmail.com>
2019-03-23 15:57:59 -0300

    aa

--- admin/class/restaurant_class.php
+++ admin/class/restaurant_class.php
@@ -396,6 +396,7 @@ class Rest{
                 $poli[] = $punto->{'lat'}." ".$punto->{'lng'};
             }
             $is = $this->pointInPolygon($lat." ".$lng, $poli);
+            $info['is'] = $is;
             if($is == "inside"){
                 if($precio > $polygon['precio']){
                     $info['op'] = 1;
