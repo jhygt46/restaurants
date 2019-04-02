a429563b952436c2e603e9ef2ae54a02b7037aae
jhygt46 <infolifeoperacional@gmail.com>
2019-04-02 14:13:51 -0300

    aa

--- admin/class/restaurant_class.php
+++ admin/class/restaurant_class.php
@@ -50,19 +50,20 @@ class Rest{
             $referer = $_POST["referer"];
         }
         $usuario = $this->con->sql("SELECT t1.nombre, t2.id_pdir, t2.calle, t2.num, t2.depto, t2.comuna, t2.lat, t2.lng FROM pedidos_usuarios t1, pedidos_direccion t2, giros t3 WHERE t3.dominio='".$referer."' AND t3.id_gir=t1.id_gir AND t1.telefono='".$telefono."' AND t1.id_puser=t2.id_puser");
-        for($i=0; $i<$usuario["count"]; $i++){
-
-            $info['nombre'] = $usuario['resultado'][$i]['nombre'];
-
-            $aux_dir["id_pdir"] = $usuario['resultado'][$i]['id_pdir'];
-            $aux_dir["calle"] = $usuario['resultado'][$i]['calle'];
-            $aux_dir["num"] = $usuario['resultado'][$i]['num'];
-            $aux_dir["depto"] = $usuario['resultado'][$i]['depto'];
-            $aux_dir["lat"] = $usuario['resultado'][$i]['lat'];
-            $aux_dir["lng"] = $usuario['resultado'][$i]['lng'];
-            
-            $info['direcciones'][] = $aux_dir;
-            unset($aux_dir);
+        $info['cantidad'] = 0;
+        if($usuario["count"] > 0){
+            $info['cantidad'] = $usuario["count"];
+            for($i=0; $i<$usuario["count"]; $i++){
+                $info['nombre'] = $usuario['resultado'][$i]['nombre'];
+                $aux_dir["id_pdir"] = $usuario['resultado'][$i]['id_pdir'];
+                $aux_dir["calle"] = $usuario['resultado'][$i]['calle'];
+                $aux_dir["num"] = $usuario['resultado'][$i]['num'];
+                $aux_dir["depto"] = $usuario['resultado'][$i]['depto'];
+                $aux_dir["lat"] = $usuario['resultado'][$i]['lat'];
+                $aux_dir["lng"] = $usuario['resultado'][$i]['lng'];
+                $info['direcciones'][] = $aux_dir;
+                unset($aux_dir);
+            }
         }
         return $info;
         
