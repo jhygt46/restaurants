b4e5f0d2febe6fce4c3f5c84348f463a25ce4c6e
jhygt46 <infolifeoperacional@gmail.com>
2019-04-08 16:59:49 -0400

    aa

--- admin/class/login_class.php
+++ admin/class/login_class.php
@@ -33,15 +33,16 @@ class Login {
 
                 $this->con->sql("UPDATE fw_usuarios SET pass='', mailcode='".$send["code"]."' WHERE id_user='".$send["id"]."'");
 
+                $info['op'] = 1;
+                $info['message'] = "Correo Enviado";
+
                 $ch = curl_init();
                 curl_setopt($ch, CURLOPT_URL, 'https://www.izusushi.cl/mail_recuperar');
                 curl_setopt($ch, CURLOPT_RETURNTRANSFER, true);
                 curl_setopt($ch, CURLOPT_POSTFIELDS, http_build_query($send));
-                curl_exec($ch);
+                $info['data'] = curl_exec($ch);
                 curl_close($ch);
 
-                $info['op'] = 1;
-                $info['message'] = "Correo Enviado";
 
             }else{
 
