202084a3d5060413912531114897fb8d3feb5013
jhygt46 <infolifeoperacional@gmail.com>
2019-04-14 11:30:40 -0400

    aa

--- js/cocina.js
+++ js/cocina.js
@@ -1,18 +1,15 @@
 $(document).ready(function(){
     socket_init();
-    console.log(data);
 });
 
 function socket_init(){
     
     var socket = io.connect('https://www.izusushi.cl', { 'secure': true });
-    socket.on('cocina-'+local_code, function(id_ped) {
-        console.log("agregar pedido: "+id_ped);
-        agregar_pedido(id_ped);
+    socket.on('cocina-'+local_code, function(id) {
+        agregar_pedido(id);
     });
     socket.on('cocina-pos-'+local_code, function(info) {
-        console.log("info");
-        console.log(info);
+        modificar_pedido(info);
     });
     socket.on('connect', function() {
         $('.alert_socket').hide();
@@ -22,8 +19,17 @@ function socket_init(){
     });
     
 }
+function modificar_pedido(info){
+
+    console.log("MOD PEDIDO POS");
+    console.log(info);
+
+}
 function agregar_pedido(id){
 
+    console.log("ADD PEDIDO: "+id);
+
+    /*
     var send = { id_ped: id };
     $.ajax({
         url: "ajax/get_pedido.php",
@@ -32,11 +38,11 @@ function agregar_pedido(id){
         success: function(data){
             
             var info = JSON.parse(data);
-            console.log(info);
             
         }, error: function(e){
             console.log(e);
         }
     });
-    
+    */
+
 }
