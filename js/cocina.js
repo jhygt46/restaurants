34fe668c5040bb52fdc644cf735e0b3957e427b3
jhygt46 <infolifeoperacional@gmail.com>
2019-04-16 18:57:23 -0400

    aa

--- js/cocina.js
+++ js/cocina.js
@@ -80,7 +80,7 @@ function listar_pedidos(){
     
     $('.lista_pedidos').html('');
     pedidos.sort(function (a, b){
-        return (b.num_ped - a.num_ped);
+        return (a.num_ped - b.num_ped);
     })
     pedidos.forEach(function(valor){
         $('.lista_pedidos').prepend(createDiv(valor));
