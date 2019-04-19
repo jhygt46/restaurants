6907783c99e8049149094fa4380512bfb64f6534
jhygt46 <infolifeoperacional@gmail.com>
2019-04-19 17:14:54 -0400

    aa

--- js/pos_lista.js
+++ js/pos_lista.js
@@ -1352,7 +1352,8 @@ function guardar_pedido(index, open){
                 pedidos[index].num_ped = info.num_ped;
                 pedidos[index].pedido_code = info.pedido_code;
             }
-            pedidos[index].carro = info.carro;
+            console.log(info.carro);
+            console.log(pedidos[index].carro);
             set_pedidos(pedidos);
             listar_pedidos();
 
