7570a8e2277f91a0ad8c962990ea6ac3b5dbf3d8
jhygt46 <infolifeoperacional@gmail.com>
2019-04-22 19:18:28 -0400

    aa

--- admin/class/restaurant_class.php
+++ admin/class/restaurant_class.php
@@ -444,7 +444,7 @@ class Rest{
             curl_setopt($ch, CURLOPT_URL, 'https://www.izusushi.cl/enviar_chat');
             curl_setopt($ch, CURLOPT_RETURNTRANSFER, true);
             curl_setopt($ch, CURLOPT_POSTFIELDS, http_build_query($pedido));
-            curl_exec($ch);
+            $info["mail_nodejs"] = json_decode(curl_exec($ch));
             curl_close($ch);
 
         }
