e28c0053ad104a814387284b72973461a55dcef1
jhygt46 <infolifeoperacional@gmail.com>
2019-04-25 15:21:04 -0400

    aa

---
+++ mail.php
@@ -0,0 +1,10 @@
+
+<div style="width: 100%">
+    <div style="width: 610px; margin: 0 auto">
+        <div style="display: block; height: 300px"><a href="https://misitiodelivery.cl" target="_blank" style=""><img src="https://misitiodelivery.cl/images/mail_01.jpg" alt="" /></a></div>
+        <div style="display: block; height: 140px; margin-top: 8px; box-shadow: 2px 2px 2px rgba(0,0,0,0.2)"><a href="https://misitiodelivery.cl/passw/#ID#/#CODE#/" target="_blank" style=""><img src="https://misitiodelivery.cl/images/mail_02.jpg" alt="" /></a></div>
+        <div style="display: block; height: 140px; margin-top: 8px; box-shadow: 2px 2px 2px rgba(0,0,0,0.2)"><a href="https://misitiodelivery.cl/video" target="_blank" style=""><img src="https://misitiodelivery.cl/images/mail_03.jpg" alt="" /></a></div>
+        <div style="display: block; height: 140px; margin-top: 8px; box-shadow: 2px 2px 2px rgba(0,0,0,0.2)"><a href="https://misitiodelivery.cl/contacto/#ID#/" target="_blank" style=""><img src="https://misitiodelivery.cl/images/mail_04.jpg" alt="" /></a></div>
+        <div style="display: block; height: 60px; background: #999"></div>
+    </div>
+</div>
