096eecd4e466212b3e92d97ba93555af14e52787
jhygt46 <infolifeoperacional@gmail.com>
2019-04-25 16:19:35 -0400

    aa

--- mail.php
+++ mail.php
@@ -5,6 +5,16 @@
         <div style="display: block; height: 140px; margin-top: 8px; box-shadow: 2px 2px 2px rgba(0,0,0,0.2)"><a href="https://misitiodelivery.cl/passw/#ID#/#CODE#/" target="_blank" style=""><img src="https://misitiodelivery.cl/images/mail_02.jpg" alt="" /></a></div>
         <div style="display: block; height: 140px; margin-top: 8px; box-shadow: 2px 2px 2px rgba(0,0,0,0.2)"><a href="https://misitiodelivery.cl/video" target="_blank" style=""><img src="https://misitiodelivery.cl/images/mail_03.jpg" alt="" /></a></div>
         <div style="display: block; height: 140px; margin-top: 8px; box-shadow: 2px 2px 2px rgba(0,0,0,0.2)"><a href="https://misitiodelivery.cl/contacto/#ID#/" target="_blank" style=""><img src="https://misitiodelivery.cl/images/mail_04.jpg" alt="" /></a></div>
-        <div style="display: block; height: 60px; background: #999"></div>
+        <div style="display: block; height: 50px; background: #999; margin-top: 8px">
+            <table cellspacing="0" cellpadding="0" border="0" height="50">
+                <tr>
+                    <td width="300"></td>
+                    <td width="220"><img src="images/siguenos.jpg" alt="" /></td>
+                    <td width="40"><a href="https://www.facebook.com/misitiodelivery" ><img src="images/facebook.jpg" alt="" /></a></td>
+                    <td width="40"><a href="https://www.instagram.com/misitiodelivery" ><img src="images/instragram.jpg" alt="" /></a></td>
+                    <td width="10"></td>
+                </tr>
+            </table>
+        </div>
     </div>
 </div>
