d598b7154b36742731313a71dbb241743b28a795
jhygt46 <infolifeoperacional@gmail.com>
2019-04-29 16:04:58 -0400

    aa

--- admin/pages/msd/configurar_contenido.php
+++ admin/pages/msd/configurar_contenido.php
@@ -9,6 +9,7 @@
     <div class="cont_pagina">
         <div class="cont_pag">
             <div class="lista_items">
+                <div class="titulo_items"><h1>Contenido</h1><h2>Agrega contenido a tu sitio</h2></div>
                 <div class="items_list clearfix">
                     <div class="list_item wi_01"><div class="cont_item" onclick="navlink('pages/msd/configurar_paginas.php')"><div class="item_image"><img src="images/configpages.png" alt="" /></div><div class="item_ttl">Paginas</div></div></div>
                     <div class="list_item wi_01"><div class="cont_item" onclick="navlink('pages/msd/configurar_footer.php')"><div class="item_image"><img src="images/configfooter.png" alt="" /></div><div class="item_ttl">Footer</div></div></div>
