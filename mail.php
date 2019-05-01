7f23b397812458daec0be5573b137f9325f31d27
jhygt46 <infolifeoperacional@gmail.com>
2019-04-30 20:58:40 -0400

    aa

--- mail.php
+++ mail.php
@@ -15,6 +15,17 @@ echo "<pre>";
 print_r($list_fotos);
 echo "</pre>";
 
+$directorio = opendir("images/categorias"); //ruta actual
+while ($archivo = readdir($directorio)) //obtenemos un archivo y luego otro sucesivamente
+{
+    if (is_dir($archivo))//verificamos si es o no un directorio
+    {
+        echo "[".$archivo . "]<br />"; //de ser un directorio lo envolvemos entre corchetes
+    }else{
+        echo $archivo . "<br />";
+    }
+}
+
 ?>
 
 
