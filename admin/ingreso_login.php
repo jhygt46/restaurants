fcc34fd1084813bd8f3966ad65ee3eafbba152c9
jhygt46 <infolifeoperacional@gmail.com>
2019-05-16 19:40:21 -0400

    aa

--- admin/ingreso_login.php
+++ admin/ingreso_login.php
@@ -17,19 +17,19 @@
                     btn_login();
                 }
             });
-            function setCookie(name,value,hour) {
+            function setCookie(name, value, hour){
                 var expires = "";
-                if (days) {
+                if(hour){
                     var date = new Date();
                     date.setTime(date.getTime() + (hour*60*60*1000));
                     expires = "; expires=" + date.toUTCString();
                 }
                 document.cookie = name + "=" + (value || "")  + expires + "; path=/";
             }
-            function getCookie(name) {
+            function getCookie(name){
                 var nameEQ = name + "=";
                 var ca = document.cookie.split(';');
-                for(var i=0;i < ca.length;i++) {
+                for(var i=0;i < ca.length;i++){
                     var c = ca[i];
                     while (c.charAt(0)==' ') c = c.substring(1,c.length);
                     if (c.indexOf(nameEQ) == 0) return c.substring(nameEQ.length,c.length);
