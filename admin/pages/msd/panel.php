014b6ff127be0103547472b500bf87a994c686f2
jhygt46 <infolifeoperacional@gmail.com>
2019-05-17 19:22:55 -0400

    aa

--- admin/pages/msd/panel.php
+++ admin/pages/msd/panel.php
@@ -21,4 +21,45 @@ echo "<pre>";
 print_r($correos);
 echo "</pre>";
 
+$titulo = "Pendientes Panel";
+$eliminaraccion = "eliminar_usuario";
+$id_list = "id_user";
+$eliminarobjeto = "Usuario";
+
 ?>
+<div class="pagina">
+    <div class="title">
+        <h1><?php echo $titulo; ?></h1>
+        <ul class="clearfix">
+            <li class="back" onclick="navlink('pages/msd/ver_giro.php')"></li>
+        </ul>
+    </div>
+    <hr>
+    <div class="cont_pagina">
+        <div class="cont_pag">
+            <div class="list_titulo clearfix">
+                <div class="titulo"><h1><?php echo $titulo_list; ?></h1></div>
+                <ul class="opts clearfix">
+                    <li class="opt">1</li>
+                    <li class="opt">2</li>
+                </ul>
+            </div>
+            <div class="listado_items">
+                <?php 
+                for($i=0; $i<count($list); $i++){
+                    $id = $list[$i][$id_list];
+                    $nombre = $list[$i]['nombre'];
+                    $dominio = $list[$i]['dominio'];
+                ?>
+                <div class="l_item">
+                    <div class="detalle_item clearfix">
+                        <div class="nombre"><?php echo $nombre; ?></div>
+                        <a class="icono ic11" onclick="eliminar('<?php echo $eliminaraccion; ?>', '<?php echo $id; ?>', '<?php echo $eliminarobjeto; ?>', '<?php echo $nombre; ?>')"></a>
+                        <a class="icono ic1" onclick="navlink('<?php echo $page_mod; ?>?id_user=<?php echo $id; ?>')"></a>
+                    </div>
+                </div>
+                <?php } ?>
+            </div>
+        </div>
+    </div>
+</div>
