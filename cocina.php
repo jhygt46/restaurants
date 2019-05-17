1bf04ea78d95ca6efa7b6b13d227c12ce433ecca
jhygt46 <infolifeoperacional@gmail.com>
2019-05-17 11:44:07 -0400

    aa

--- cocina.php
+++ cocina.php
@@ -4,6 +4,9 @@ date_default_timezone_set('America/Santiago');
 require('admin/class/core_class.php');
 $core = new Core();
 
+
+echo "HOLA";
+exit;
 /*
 $id_loc = (is_numeric($_GET["id_loc"])) ? $_GET["id_loc"] : 0 ;
 $local = $core->local($id_loc);
