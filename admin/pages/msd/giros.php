dd56ec7fb6324aedf820daa08d9481da6ac36dc2
jhygt46 <infolifeoperacional@gmail.com>
2019-06-13 19:32:13 -0400

    aa

--- admin/pages/msd/giros.php
+++ admin/pages/msd/giros.php
@@ -26,6 +26,7 @@ if($core->admin == 1){
     $id_gir = 0;
     $sub_titulo = $sub_titulo1;
     $class = ($_POST['w'] < 600) ? 'resp' : 'normal' ;
+    $that = ["nombre" => "", "dominio" => "", "item_pagina" => 0, "item_pos" => 0, "item_cocina" => 0, "item_grafico" => 0, "dns_letra" => ""];
     if(isset($_GET["id_gir"]) && is_numeric($_GET["id_gir"]) && $_GET["id_gir"] != 0){
         
         $sub_titulo = $sub_titulo2;
