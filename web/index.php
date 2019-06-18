affb5489569fdcdb0f0965eecd29b3ca7ca88645
jhygt46 <infolifeoperacional@gmail.com>
2019-06-18 17:27:03 -0400

    aa

---
+++ web/index.php
@@ -0,0 +1,13 @@
+<?php
+
+require_once "/var/www/html/restaurants/admin/class/core_class_prod.php";
+$core = new Core();
+
+if($_POST["tipo"] == 1){
+    echo json_encode($core->get_web_js_data_remote());
+}
+if($_POST["tipo"] == 2){
+    echo json_encode($core->enviar_pedido());
+}
+
+?>
