fb4972e2754003094966ffc4244a75545dffdf3a
jhygt46 <infolifeoperacional@gmail.com>
2019-06-24 22:58:56 -0400

    aa

--- admin/class/core_class_prod.php
+++ admin/class/core_class_prod.php
@@ -1214,7 +1214,7 @@ class Core{
         $user_code = $_COOKIE["user_code"];
         $local_code = $_COOKIE["local_code"];
 
-        $sql = $this->con->prepare("SELECT t2.id_loc, t2.lat, t2.lng, t2.sonido FROM fw_usuarios t1, locales t2, giros t3 WHERE t1.id_user=? AND t1.cookie_code=? AND t1.id_loc=t2.id_loc AND t2.cookie_code=? AND cookie_ip=? AND t2.id_gir=t3.id_gir AND t1.eliminado=? AND t2.eliminado=? AND t3.eliminado=?");
+        $sql = $this->con->prepare("SELECT t2.id_loc, t2.lat, t2.lng, t2.sonido, t3.estados FROM fw_usuarios t1, locales t2, giros t3 WHERE t1.id_user=? AND t1.cookie_code=? AND t1.id_loc=t2.id_loc AND t2.cookie_code=? AND cookie_ip=? AND t2.id_gir=t3.id_gir AND t1.eliminado=? AND t2.eliminado=? AND t3.eliminado=?");
         $sql->bind_param("isssiii", $id, $user_code, $local_code, $ip, $this->eliminado, $this->eliminado, $this->eliminado);
         $sql->execute();
         $res = $sql->get_result();
@@ -1233,6 +1233,7 @@ class Core{
             $info['lat'] = $result['lat'];
             $info['lng'] = $result['lng'];
             $info['sonido'] = $result['sonido'];
+            $info['estados'] = $result['estados'];
             /*
             $info['code'] = $result['code'];
             $info['nombre'] = $result['nombre'];
--- admin/punto_de_venta/index.php
+++ admin/punto_de_venta/index.php
@@ -29,7 +29,7 @@ $info = $core->get_data_pos();
             //var ssl = '<?php echo $info['ssl']; ?>';
             //var dominio = '<?php echo $info['dominio']; ?>';
             //var tiempos = { retiro: <?php echo $info['t_retiro']; ?>, despacho: <?php echo $info['t_despacho']; ?> };
-            //var estados = [ <?php for($i=0; $i<count($info['estados']); $i++){ if($i>0){ echo ", "; } echo "'".$info['estados'][$i]."'";  } ?> ];
+            var estados = [ <?php for($i=0; $i<count($info['estados']); $i++){ if($i>0){ echo ", "; } echo "'".$info['estados'][$i]."'";  } ?> ];
             var pedidos = <?php if($info["pedidos"] != null){ echo json_encode($info["pedidos"]); }else{ echo '[]'; } ?>;
             var motos = <?php if($info["motos"] != null){ echo json_encode($info["motos"]); }else{ echo '[]'; } ?>;
         </script>
