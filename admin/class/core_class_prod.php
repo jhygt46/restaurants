5f847bd2601ed275a460ffdc12d302737749bd2c
jhygt46 <infolifeoperacional@gmail.com>
2019-06-25 19:00:20 -0400

    aa

--- admin/class/core_class_prod.php
+++ admin/class/core_class_prod.php
@@ -1479,10 +1479,10 @@ class Core{
         $local_code = $_COOKIE["local_code"];
 
 
-        $sql = $this->con->prepare("SELECT * FROM fw_usuarios t1, locales t2 WHERE t1.id_user=? AND t1.cookie_code=? AND t1.id_loc=t2.id_loc AND t2.cookie_code=? AND t2.cookie_ip=?");
-        $sql->bind_param("isss", $id, $user_code, $local_code, $ip);
+        $sql = $this->con->prepare("SELECT t1.id_user FROM fw_usuarios t1, locales t2 WHERE t1.id_user=? AND t1.cookie_code=? AND t1.id_loc=t2.id_loc AND t2.cookie_code=? AND t2.cookie_ip=? AND t1.eliminado=? AND t2.eliminado=?");
+        $sql->bind_param("isssii", $id, $user_code, $local_code, $ip, $this->eliminado, $this->eliminado);
         $sql->execute();
-        $res = $sql->get_result();
+        $res = $sql->get_result()->fetch_all(MYSQLI_ASSOC)[0];
         $sql->free_result();
         $sql->close();
 
