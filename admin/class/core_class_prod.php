cbd378c641bc82c2b354952bdf5a16e21cbf6c44
jhygt46 <infolifeoperacional@gmail.com>
2019-06-27 23:32:15 -0400

    aa

--- admin/class/core_class_prod.php
+++ admin/class/core_class_prod.php
@@ -1112,12 +1112,12 @@ class Core{
         $sql = $this->con->prepare("SELECT t1.id_ped, t1.num_ped, t1.id_loc, t3.ssl, t3.code, t1.id_ped, t1.id_puser, t1.id_pdir, t1.despacho, t1.carro, t1.promos, t1.pre_wasabi, t1.pre_gengibre, t1.pre_embarazadas, t1.pre_soya, t1.pre_teriyaki, t1.pre_palitos, t1.comentarios, t1.costo, t1.total, t1.verify_despacho FROM pedidos_aux t1, locales t2, giros t3 WHERE t1.code=? AND t1.id_loc=t2.id_loc AND t2.id_gir=t3.id_gir AND t3.dominio=? AND t1.eliminado=? AND t1.fecha > DATE_ADD(NOW(), INTERVAL -2 DAY)");
         $sql->bind_param("isi", $code, $host, $this->eliminado);
         $sql->execute();
-        $sql->store_result();
+        $res = $sql->get_result();
         $info['op'] = 2;
 
-        if($sql->{"num_rows"} == 1){
+        if($res->{"num_rows"} == 1){
 
-            $result = $sql->get_result()->fetch_all(MYSQLI_ASSOC)[0];
+            $result = $res->fetch_all(MYSQLI_ASSOC)[0];
             $sql->free_result();
             $sql->close();
 
@@ -1144,7 +1144,7 @@ class Core{
 
             }
             */
-            
+
             $info['id_ped'] = $result['id_ped'];
             $info['num_ped'] = $result['num_ped'];
             $info['carro'] = $result['carro'];
