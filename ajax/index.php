c854805083fc1ac5c3763144ebb37f8f19184cd9
jhygt46 <infolifeoperacional@gmail.com>
2019-06-28 23:38:17 -0400

    aa

--- ajax/index.php
+++ ajax/index.php
@@ -2,7 +2,7 @@
 
     if($_POST["accion"] == "despacho_domicilio"){
         require_once "../admin/class/restaurant_class.php";
-        $core = new Core();
+        $core = new Rest();
         $info = $core->get_info_despacho($_POST["lat"], $_POST["lng"]);
         echo json_encode($info);
     }
