b6b52fb997c05b41b07bf9c395048593cc02cf99
jhygt46 <infolifeoperacional@gmail.com>
2019-06-29 23:20:18 -0400

    aa

--- js/borrar.js
+++ js/borrar.js
@@ -1378,25 +1378,7 @@ function ocultar_pedido(){
     $('.pop_up').hide();
 }
 */
-var formatNumber = {
-    separador: ".", // separador para los miles
-    sepDecimal: ',', // separador para los decimales
-    formatear:function (num){
-        num +='';
-        var splitStr = num.split('.');
-        var splitLeft = splitStr[0];
-        var splitRight = splitStr.length > 1 ? this.sepDecimal + splitStr[1] : '';
-        var regx = /(\d+)(\d{3})/;
-        while (regx.test(splitLeft)) {
-            splitLeft = splitLeft.replace(regx, '$1' + this.separador + '$2');
-        }
-        return this.simbol + splitLeft +splitRight;
-    },
-    new: function(num, simbol){
-        this.simbol = simbol ||'';
-        return this.formatear(num);
-    }
-}
+
 function telefono_keyup(e){
 
     var len = e.value;
