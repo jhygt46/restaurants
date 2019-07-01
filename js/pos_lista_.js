81a8948d86cdf74bd8dacb5c8554750ff0c47bd0
jhygt46 <infolifeoperacional@gmail.com>
2019-07-01 15:06:45 -0400

    aa

--- js/pos_lista_.js
+++ js/pos_lista_.js
@@ -570,3 +570,25 @@ function np_close(that){
     $(that).parents('.pop').hide();
     
 }
+function add_carro_producto(id_pro){
+
+    var pedidos = get_pedidos();
+    var producto = get_producto(id_pro);
+    var item_carro = { id_pro: parseInt(id_pro) };
+    
+    if(producto.preguntas){
+        item_carro.preguntas = [];
+        for(var k=0, klen=producto.preguntas.length; k<klen; k++){
+            item_carro.preguntas.push(get_preguntas(producto.preguntas[k]));
+        }
+    }
+
+    pedidos[seleccionado].total = parseInt(pedidos[seleccionado].total) + parseInt(producto.precio);
+    pedidos[seleccionado].carro.push(item_carro);
+    if(producto.preguntas){
+        mostrar_pregunta(pedidos[seleccionado].carro.length - 1);
+    }
+
+    listar_pedidos(pedidos);
+
+}
