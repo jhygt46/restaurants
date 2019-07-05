af96f52fbfe1e030156a71294b4f34253f71a250
jhygt46 <infolifeoperacional@gmail.com>
2019-07-04 22:25:20 -0400

    aa

--- js/pos_lista_.js
+++ js/pos_lista_.js
@@ -996,6 +996,7 @@ function select_pdir(that){
                 $('#id_pdir').val($(that).parent().attr('id_pdir'));
                 $('#direccion').val($(that).parent().attr('direccion'));
                 $('#depto').val($(that).parent().attr('depto'));
+                $('#despacho').val(1);
                 $('#costo').val(data.precio);
                 $('#l_direccion').hide();
 
