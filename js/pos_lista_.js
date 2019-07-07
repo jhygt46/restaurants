fe97efe1ed60aec8869b7020f06ef38a4fbebc7c
jhygt46 <infolifeoperacional@gmail.com>
2019-07-07 17:41:05 -0400

    aa

--- js/pos_lista_.js
+++ js/pos_lista_.js
@@ -754,7 +754,7 @@ function confirmar_pregunta_productos(that){
 
                 //pedidos[seleccionado].carro[i].preguntas[k].valores[m].seleccionados = valores;
                 console.log(seleccionado+"-"+i+"-"+k+"-"+m);
-                console.log(pedidos[seleccionado].carro[i]);
+                console.log(pedidos[seleccionado].carro[i].preguntas[k].valores[m]);
                 var t_pregunta = -1;
                 for(var m=0, mlen=pedidos[seleccionado].carro.length; m<mlen; m++){
                     if(tiene_pregunta(pedidos[seleccionado].carro[m])){
