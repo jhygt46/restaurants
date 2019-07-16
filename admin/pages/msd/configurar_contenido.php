8ded58dfb722e47cb5ec691dbc5b0e38e74d8c75
jhygt46 <infolifeoperacional@gmail.com>
2019-07-15 20:49:14 -0400

    aa

--- admin/pages/msd/configurar_contenido.php
+++ admin/pages/msd/configurar_contenido.php
@@ -13,6 +13,7 @@
                 <div class="items_list clearfix">
                     <div class="list_item wi_01"><div class="cont_item" onclick="navlink('pages/msd/configurar_paginas.php')"><div class="item_image"><img src="images/configpages.png" alt="" /></div><div class="item_ttl">Paginas</div></div></div>
                     <div class="list_item wi_01"><div class="cont_item" onclick="navlink('pages/msd/configurar_footer.php')"><div class="item_image"><img src="images/configfooter.png" alt="" /></div><div class="item_ttl">Footer</div></div></div>
+                    <div class="list_item wi_01"><div class="cont_item" onclick="navlink('pages/msd/configurar_pag_inicio.php')"><div class="item_image"><img src="images/configpaginicio.png" alt="" /></div><div class="item_ttl">Inicio</div></div></div>
                 </div>
             </div>
         </div>
