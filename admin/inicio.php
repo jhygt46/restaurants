8cb3e4a1cb5bd235c0deb0c15932c28c014ffb05
jhygt46 <infolifeoperacional@gmail.com>
2019-10-02 16:26:17 -0300

    aa

--- admin/inicio.php
+++ admin/inicio.php
@@ -1,5 +1,16 @@
 <?php
 
+    if($_SERVER["HTTP_HOST"] == "localhost"){
+        define("DIR_BASE", $_SERVER["DOCUMENT_ROOT"]."/");
+        define("DIR", DIR_BASE."restaurants/");
+    }else{
+        define("DIR_BASE", "/var/www/html/");
+        define("DIR", DIR_BASE."restaurants/");
+    }
+
+    require_once DIR."admin/class/core_class_prod.php";
+    $core = new Core();
+
     $inicio = $core->inicio();
 
 ?>
