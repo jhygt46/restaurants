ed572e30071debe51c8089ffba829532f88b00b4
jhygt46 <infolifeoperacional@gmail.com>
2019-10-02 16:22:45 -0300

    aa

--- admin/index.php
+++ admin/index.php
@@ -1,17 +1,13 @@
 <?php
 session_start();
 
-echo "<pre>";
-print_r($_SESSION);
-echo "</pre>";
-
 if(isset($_GET["accion"]) && $_GET["accion"] == "logout"){
     session_destroy();
     echo '<meta http-equiv="refresh" content="0; url=index.php">';
     exit;
 }
 
-if(!isset($_SESSION['user']['info']['id_usr'])){
+if(!isset($_SESSION['user']['info']['id_user'])){
     include("ingreso_login.php");
 }else{
     include("inicio.php");
