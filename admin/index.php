755187e579e40ccd8a02b1eda4cf7c5f680718bf
jhygt46 <infolifeoperacional@gmail.com>
2019-10-02 16:21:47 -0300

    aa

--- admin/index.php
+++ admin/index.php
@@ -1,6 +1,10 @@
 <?php
 session_start();
 
+echo "<pre>";
+print_r($_SESSION);
+echo "</pre>";
+
 if(isset($_GET["accion"]) && $_GET["accion"] == "logout"){
     session_destroy();
     echo '<meta http-equiv="refresh" content="0; url=index.php">';
