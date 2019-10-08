8a1a6c91929ea218a8ced2c26bd1f821f62af654
jhygt46 <infolifeoperacional@gmail.com>
2019-10-08 15:23:11 -0300

    aa

--- ajax/index.php
+++ ajax/index.php
@@ -1,13 +1,20 @@
 <?php
 
+    if($_SERVER["HTTP_HOST"] == "localhost"){
+        define("DIR_BASE", $_SERVER["DOCUMENT_ROOT"]."/");
+        define("DIR", DIR_BASE."restaurants/");
+    }else{
+        define("DIR_BASE", "/var/www/html/");
+        define("DIR", DIR_BASE."restaurants/");
+    }
     if($_POST["accion"] == "despacho_domicilio"){
-        require_once "../admin/class/restaurant_class.php";
+        require_once DIR."admin/class/restaurant_class.php";
         $core = new Rest();
         $info = $core->get_info_despacho($_POST["lat"], $_POST["lng"]);
         echo json_encode($info);
     }
     if($_POST["accion"] == "get_users_pedido"){
-        require_once "../admin/class/restaurant_class.php";
+        require_once DIR."admin/class/restaurant_class.php";
         $core = new Rest();
         $info = $core->get_users_pedido($_POST["telefono"]);
         echo json_encode($info);
--- web/index.php
+++ web/index.php
@@ -1,18 +1,31 @@
 <?php
 
-require_once "/var/www/html/restaurants/admin/class/core_class_prod.php";
-$core = new Core();
+if($_SERVER["HTTP_HOST"] == "localhost"){
+    define("DIR_BASE", $_SERVER["DOCUMENT_ROOT"]."/");
+    define("DIR", DIR_BASE."restaurants/");
+}else{
+    define("DIR_BASE", "/var/www/html/");
+    define("DIR", DIR_BASE."restaurants/");
+}
 
 if($_POST["tipo"] == 1){
+    require_once DIR."admin/class/core_class_prod.php";
+    $core = new Core();
     echo json_encode($core->get_web_js_data_remote());
 }
 if($_POST["tipo"] == 2){
+    require_once DIR."admin/class/core_class_prod.php";
+    $core = new Core();
     echo json_encode($core->enviar_pedido());
 }
 if($_POST["tipo"] == 3){
+    require_once DIR."admin/class/core_class_prod.php";
+    $core = new Core();
     echo json_encode($core->enviar_error());
 }
 if($_POST["tipo"] == 4){
+    require_once DIR."admin/class/core_class_prod.php";
+    $core = new Core();
     echo json_encode($core->ver_detalle());
 }
 
