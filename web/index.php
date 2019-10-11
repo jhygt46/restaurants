6342264f1b378a66ceb25a7a18d64f5e715d23e6
jhygt46 <infolifeoperacional@gmail.com>
2019-10-11 18:59:11 -0300

    aa

--- web/index.php
+++ web/index.php
@@ -15,22 +15,35 @@ if($file[count($file) - 1] != ""){
     exit;
 }
 
+if(isset($_POST["tipo"])){
 
+    if($_POST["tipo"] == 1 || $_POST["tipo"] == 2 || $_POST["tipo"] == 3 || $_POST["tipo"] == 4){
+        require_once DIR."admin/class/core_class_prod.php";
+        $core = new Core();
+        if($_POST["tipo"] == 1){
+            echo json_encode($core->get_web_js_data_remote());
+        }
+        if($_POST["tipo"] == 2){
+            echo json_encode($core->enviar_pedido());
+        }
+        if($_POST["tipo"] == 3){
+            echo json_encode($core->enviar_error());
+        }
+        if($_POST["tipo"] == 4){
+            echo json_encode($core->ver_detalle());
+        }
+    }else{
+        header('HTTP/1.1 404 Not Found', true, 404);
+        include(DIR.'errors/404.html');
+        exit;
+    }
 
-require_once DIR."admin/class/core_class_prod.php";
-$core = new Core();
+}else{
 
-if($_POST["tipo"] == 1){
-    echo json_encode($core->get_web_js_data_remote());
-}
-if($_POST["tipo"] == 2){
-    echo json_encode($core->enviar_pedido());
-}
-if($_POST["tipo"] == 3){
-    echo json_encode($core->enviar_error());
-}
-if($_POST["tipo"] == 4){
-    echo json_encode($core->ver_detalle());
+    header('HTTP/1.1 404 Not Found', true, 404);
+    include(DIR.'errors/404.html');
+    exit;
+    
 }
 
 ?>
