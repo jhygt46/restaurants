13a5ab13c5425a103a91e7f3ae3e8b4a04c08dad
jhygt46 <infolifeoperacional@gmail.com>
2019-10-13 00:36:22 -0300

    aa

--- admin/class/core_class_prod.php
+++ admin/class/core_class_prod.php
@@ -1819,11 +1819,13 @@ class Core{
                                 if($sql->execute()){
                                     $info['op'] = 1;
                                     $info['id_gir'] = $id_gir;
-                                    /*
                                     $result = $sql->get_result();
                                     while($row = $result->fetch_assoc()){
-                                        $info['data']['catalogos'][] = $this->get_info_catalogo($row['id_cat']);
+                                        //$info['data']['catalogos'][] = $this->get_info_catalogo($row['id_cat']);
+                                        $info['data']['catalogos'][] = $row['id_cat'];
                                     }
+                                    /*
+                                    
                                     $info['data']['paginas'] = $this->get_paginas_web($id_gir);
                                     $info['data']['config'] = $this->get_config($id_gir);
                                     $info['data']['locales'] = $this->get_locales_js($id_gir);
