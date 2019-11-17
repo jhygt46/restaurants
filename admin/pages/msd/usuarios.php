d8ee2345333e2e4ca0368cd6bbc0b611192e2b67
jhygt46 <infolifeoperacional@gmail.com>
2019-11-16 22:12:10 -0300

    aa

--- admin/pages/msd/usuarios.php
+++ admin/pages/msd/usuarios.php
@@ -10,6 +10,10 @@ if($_SERVER["HTTP_HOST"] == "localhost"){
 
 require_once DIR."admin/class/core_class_prod.php";
 $core = new Core();
+echo "<pre>";
+print_r($core->list_arbol_cats_prods());
+echo "</pre>";
+exit;
 
 if($core->id_user == 1 || $core->re_venta == 1){
 
