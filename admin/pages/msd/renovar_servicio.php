542efb656469677117d3250366c809b0bcc6ec23
jhygt46 <infolifeoperacional@gmail.com>
2019-11-27 21:29:45 -0300

    aa

--- admin/pages/msd/renovar_servicio.php
+++ admin/pages/msd/renovar_servicio.php
@@ -26,9 +26,9 @@ $core = new Core();
             <div class="lista_items">
                 <div class="titulo_items"><h1>Seleccionar Plazos de Pago</h1><h2>Mensual, Semestral o Anual</h2></div>
                 <div class="items_list clearfix">
-                    <div class="list_item wi_01"><div class="cont_item" onclick="navlink('pages/msd/configurar_paginas.php')"><div class="item_image"><img src="images/configpages.png" alt="" /></div><div class="item_ttl">Paginas</div></div></div>
-                    <div class="list_item wi_01"><div class="cont_item" onclick="navlink('pages/msd/configurar_footer.php')"><div class="item_image"><img src="images/configfooter.png" alt="" /></div><div class="item_ttl">Footer</div></div></div>
-                    <div class="list_item wi_01"><div class="cont_item" onclick="navlink('pages/msd/configurar_pag_inicio.php')"><div class="item_image"><img src="images/configpaginicio.png" alt="" /></div><div class="item_ttl">Inicio</div></div></div>
+                    <div class="list_item wi_01"><div class="cont_item" onclick="navlink('pages/msd/configurar_paginas.php')"><div class="item_image"><img src="images/configpages.png" alt="" /></div><div class="item_ttl">Mensual</div></div></div>
+                    <div class="list_item wi_01"><div class="cont_item" onclick="navlink('pages/msd/configurar_footer.php')"><div class="item_image"><img src="images/configfooter.png" alt="" /></div><div class="item_ttl">Semestral</div></div></div>
+                    <div class="list_item wi_01"><div class="cont_item" onclick="navlink('pages/msd/configurar_pag_inicio.php')"><div class="item_image"><img src="images/configpaginicio.png" alt="" /></div><div class="item_ttl">Anual</div></div></div>
                 </div>
             </div>
         </div>
