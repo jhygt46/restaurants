990d6b516fa6173ccc3a3a674f829937be5e196a
jhygt46 <infolifeoperacional@gmail.com>
2019-11-28 14:12:00 -0300

    aa

--- admin/pages/msd/renovar_servicio_detalle.php
+++ admin/pages/msd/renovar_servicio_detalle.php
@@ -11,12 +11,16 @@ if($_SERVER["HTTP_HOST"] == "localhost"){
 require_once DIR."admin/class/core_class_prod.php";
 $core = new Core();
 $monto = $core->get_monto();
+$iva = 1.19;
 
+if($_GET["pago"] == 1){
+    $monto = $monto * $iva;
+}
 if($_GET["pago"] == 2){
-    $monto = $monto * 5.5;
+    $monto = $monto * 5.5 * $iva;
 }
 if($_GET["pago"] == 3){
-    $monto = $monto * 10;
+    $monto = $monto * 10 * $iva;
 }
 
 echo "((".$monto."))";
@@ -34,8 +38,17 @@ echo "((".$monto."))";
     <div class="cont_pagina">
         <div class="cont_pag">
             <div class="lista_items">
-                <div class="titulo_items" style="padding-bottom: 10px"><h1>Datos para transferencia electronica</h1></div>
-                
+                <div class="titulo_items"><h1>Datos para transferencia electronica</h1></div>
+                <div class="detalle_transferencia">
+                    <h1>Banco</h1>
+                    <h2>Santader Santiago</h2>
+                    <h1>Rut</h1>
+                    <h2>15.935.774-0</h2>
+                    <h1>Tipo</h1>
+                    <h2>Cuenta corriente</h2>
+                    <h1>Monto</h1>
+                    <h2><?php echo $monto; ?></h2>
+                </div>
             </div>
         </div>
     </div>
