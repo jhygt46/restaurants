f538dbe76b4c91b81a229f5c3e26a100137a0a4a
jhygt46 <infolifeoperacional@gmail.com>
2019-11-27 21:28:02 -0300

    aa

--- admin/pages/msd/renovar_servicio.php
+++ admin/pages/msd/renovar_servicio.php
@@ -15,7 +15,7 @@ $core = new Core();
 
 <div class="pagina">
     <div class="title">
-        <h1>Renovar Servicio</h1>
+        <h1>Renovación del Servicio</h1>
         <ul class="clearfix">
             <li class="back" onclick="navlink('pages/msd/ver_giro.php')"></li>
         </ul>
@@ -24,7 +24,7 @@ $core = new Core();
     <div class="cont_pagina">
         <div class="cont_pag">
             <div class="lista_items">
-                <div class="titulo_items"><h1>Contenido</h1><h2>Agrega contenido a tu sitio</h2></div>
+                <div class="titulo_items"><h1>Seleccionar Plazos de Pago</h1><h2>Mensual, Semestral o Anual</h2></div>
                 <div class="items_list clearfix">
                     <div class="list_item wi_01"><div class="cont_item" onclick="navlink('pages/msd/configurar_paginas.php')"><div class="item_image"><img src="images/configpages.png" alt="" /></div><div class="item_ttl">Paginas</div></div></div>
                     <div class="list_item wi_01"><div class="cont_item" onclick="navlink('pages/msd/configurar_footer.php')"><div class="item_image"><img src="images/configfooter.png" alt="" /></div><div class="item_ttl">Footer</div></div></div>
