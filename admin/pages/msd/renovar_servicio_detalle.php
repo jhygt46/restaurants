8938d2dc947a9d3b480e172b0bcdd8890f9d472f
jhygt46 <infolifeoperacional@gmail.com>
2019-11-28 14:28:22 -0300

    aa

--- admin/pages/msd/renovar_servicio_detalle.php
+++ admin/pages/msd/renovar_servicio_detalle.php
@@ -44,23 +44,8 @@ if($_GET["pago"] == 3){
                     <h2>15.935.774-0</h2>
                     <h1>Tipo</h1>
                     <h2>Cuenta corriente</h2>
-                    <h1>Monto</h1>
-                    <h2><?php echo $monto; ?></h2>
-                </div>
-            </div>
-        </div>
-    </div>
-    <div class="cont_pagina">
-        <div class="cont_pag">
-            <div class="lista_items">
-                <div class="titulo_items"><h1>Datos para transferencia electronica</h1></div>
-                <div class="detalle_transferencia">
-                    <h1>Banco</h1>
-                    <h2>Santader Santiago</h2>
-                    <h1>Rut</h1>
-                    <h2>15.935.774-0</h2>
-                    <h1>Tipo</h1>
-                    <h2>Cuenta corriente</h2>
+                    <h1>Correo</h1>
+                    <h2>misitiodelivery@gmail.com</h2>
                     <h1>Monto</h1>
                     <h2><?php echo $monto; ?></h2>
                 </div>
