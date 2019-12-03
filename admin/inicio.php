b3ed446ec5a0683367f8299595821288ac8e15d5
jhygt46 <infolifeoperacional@gmail.com>
2019-12-03 19:14:22 -0300

    aa

--- admin/inicio.php
+++ admin/inicio.php
@@ -111,14 +111,24 @@
                                     <ul class="bloque_lista">
                                         <li onclick="navlink('pages/msd/ver_giro.php')">Inicio<!--<p class="valign">3</p>--></li>
                                         <?php if($inicio['admin'] == 1){ ?><li onclick="navlink('pages/msd/giros.php')">Giros</li><?php } ?>
-                                        <?php if($inicio['id_user'] == 1 || $inicio['re_venta'] == 1){ ?><li onclick="navlink('pages/msd/usuarios.php')">Usuarios</li><?php } ?>
-                                        <?php if($inicio['id_user'] == 1){ ?><li onclick="navlink('pages/msd/panel.php')">Panel de Control</li><?php } ?>
-                                        <?php if($inicio['id_user'] == 1){ ?><li onclick="navlink('pages/msd/pagos.php')">Pagos</li><?php } ?>
-                                        <?php if($inicio['id_user'] == 1){ ?><li onclick="navlink('pages/msd/pagar_proveedores.php')">Pagar Proveedores</li><?php } ?>
                                         <?php if($inicio['admin'] == 1){ ?><li onclick="navlink('pages/msd/cuentas.php')">Cuentas</li><?php } ?>
                                         <?php if($inicio['admin'] == 1){ ?><li onclick="navlink('pages/msd/ver_disponibilidad.php')">Disponibilidad</li><?php } ?>
                                     </ul>
                                 </div>
+                                <?php if($inicio['id_user'] == 1 || $inicio['re_venta'] == 1){ ?>
+                                <div class="bloque">
+                                    <div class="titulo" onclick="open_bloque(this)">
+                                        <div class="icono ic1"></div>
+                                        <div class="texto">Administrador</div>
+                                    </div>
+                                    <ul class="bloque_lista">
+                                        <?php if($inicio['id_user'] == 1 || $inicio['re_venta'] == 1){ ?><li onclick="navlink('pages/msd/usuarios.php')">Usuarios</li><?php } ?>
+                                        <?php if($inicio['id_user'] == 1){ ?><li onclick="navlink('pages/msd/panel.php')">Panel de Control</li><?php } ?>
+                                        <?php if($inicio['id_user'] == 1){ ?><li onclick="navlink('pages/msd/pagos.php')">Pagos</li><?php } ?>
+                                        <?php if($inicio['id_user'] == 1){ ?><li onclick="navlink('pages/msd/pagar_proveedores.php')">Pagar Proveedores</li><?php } ?>
+                                    </ul>
+                                </div>
+                                <?php } ?>
                             </div>                            
                         </div>
                     </div>
