c23a256b5d57239aa9bbb6994ebaf030246676fd
jhygt46 <infolifeoperacional@gmail.com>
2019-12-03 16:48:13 -0300

    aa

--- admin/pages/msd/ver_mapa.php
+++ admin/pages/msd/ver_mapa.php
@@ -13,7 +13,42 @@ $core = new Core();
 
 
 ?>
+<script>
 
+    var map, infoWindow;
+    function initMap(){
+
+        map = new google.maps.Map(document.getElementById('map'), {
+          center: {lat: -34.397, lng: 150.644},
+          zoom: 6
+        });
+        infoWindow = new google.maps.InfoWindow;
+
+        if (navigator.geolocation){
+            navigator.geolocation.getCurrentPosition(function(position){
+                var pos = {
+                    lat: position.coords.latitude,
+                    lng: position.coords.longitude
+                };
+                infoWindow.setPosition(pos);
+                infoWindow.setContent('Location found.');
+                infoWindow.open(map);
+                map.setCenter(pos);
+            }, function() {
+                handleLocationError(true, infoWindow, map.getCenter());
+            });
+        }else{
+          handleLocationError(false, infoWindow, map.getCenter());
+        }
+    }
+
+    function handleLocationError(browserHasGeolocation, infoWindow, pos){
+        infoWindow.setPosition(pos);
+        infoWindow.setContent(browserHasGeolocation ? 'Error: The Geolocation service failed.' : 'Error: Your browser doesn\'t support geolocation.');
+        infoWindow.open(map);
+    }
+
+</script>
 <div class="pagina">
     <div class="title">
         <h1>Mapa de Restaurants</h1>
@@ -23,7 +58,7 @@ $core = new Core();
         <div class="cont_pag">
             <div class="lista_items">
                 <div class="titulo_items" style="padding-bottom: 10px"><h1>Posicion de restaurantes existantes</h1></div>
-                <div id="mapa" style="width: 100%; height: 500px; background: #f00"></div>
+                <div id="map" style="width: 100%; height: 500px; background: #f00"></div>
             </div>
         </div>
     </div>
