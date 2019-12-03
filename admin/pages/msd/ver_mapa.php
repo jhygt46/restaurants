2f6cf9faa1b496e3dc0dbe5f72a47937c6f4290d
jhygt46 <infolifeoperacional@gmail.com>
2019-12-03 16:44:48 -0300

    aa

--- admin/pages/msd/ver_mapa.php
+++ admin/pages/msd/ver_mapa.php
@@ -10,53 +10,20 @@ if($_SERVER["HTTP_HOST"] == "localhost"){
 
 require_once DIR."admin/class/core_class_prod.php";
 $core = new Core();
-$monto = $core->get_monto();
-$monto2 = $monto * 5.5;
-$monto3 = $monto * 10;
+
 
 ?>
 
 <div class="pagina">
     <div class="title">
-        <h1>Renovación del servicio</h1>
-        <ul class="clearfix">
-            <li class="back" onclick="navlink('pages/msd/ver_giro.php')"></li>
-        </ul>
+        <h1>Mapa de Restaurants</h1>
     </div>
     <hr>
     <div class="cont_pagina">
         <div class="cont_pag">
             <div class="lista_items">
-                <div class="titulo_items" style="padding-bottom: 10px"><h1>Elije el plazo que mas te acomode</h1></div>
-                <div class="planes clearfix">
-                    <div class="plan">
-                        <div class="detalleplan">
-                            <div class="cont_detalle" onclick="navlink('pages/msd/renovar_servicio_detalle.php?pago=1')">
-                                <h1>Plan<br/>Mensual</h1>
-                                <h2>No hay ahorro</h2>
-                                <h3>$<?php echo number_format($monto, 0, '', '.'); ?> + iva</h3>
-                            </div>
-                        </div>
-                    </div>
-                    <div class="plan">
-                        <div class="detalleplan">
-                            <div class="cont_detalle" onclick="navlink('pages/msd/renovar_servicio_detalle.php?pago=2')">
-                                <h1>Plan<br/>Semestral</h1>
-                                <h2>Ahorra 1 mes al año</h2>
-                                <h3>$<?php echo number_format($monto2, 0, '', '.'); ?> + iva</h3>
-                            </div>
-                        </div>
-                    </div>
-                    <div class="plan">
-                        <div class="detalleplan">
-                            <div class="cont_detalle" onclick="navlink('pages/msd/renovar_servicio_detalle.php?pago=3')">
-                                <h1>Plan<br/>Anual</h1>
-                                <h2>Ahorra 2 meses al año</h2>
-                                <h3>$<?php echo number_format($monto3, 0, '', '.'); ?> + iva</h3>
-                            </div>
-                        </div>
-                    </div>
-                </div>
+                <div class="titulo_items" style="padding-bottom: 10px"><h1>Posicion de restaurantes existantes</h1></div>
+                <div id="mapa" style="width: 100%; height: 500px; background: #f00"></div>
             </div>
         </div>
     </div>
