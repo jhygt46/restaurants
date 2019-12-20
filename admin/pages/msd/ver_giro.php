e1eb671dc3750ba015ff4dbc9f1b9b57029bb28e
jhygt46 <infolifeoperacional@gmail.com>
2019-12-20 20:45:06 -0300

    aa

--- admin/pages/msd/ver_giro.php
+++ admin/pages/msd/ver_giro.php
@@ -17,7 +17,7 @@ if(!isset($core_class_iniciada)){
 $core->is_giro();
 
 echo "<pre>";
-print_r($core->get_aux_promo(1, $core->id_gir));
+print_r($core->get_aux_promo(1));
 echo "</pre>";
 
 // SOLO GIROS
